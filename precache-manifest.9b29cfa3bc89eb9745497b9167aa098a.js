b8a31e1aff3b65e068312f02b16934bc16ed0b3a
Tiger Cao <tigercao1@gmail.com>
2020-10-04 13:21:50 -0400

    Updates

---
+++ precache-manifest.9b29cfa3bc89eb9745497b9167aa098a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04da12d2cc327e17704de51632d7b32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a089ca6d9fc228b79e1f",
+    "url": "/static/css/main.246c11b9.chunk.css"
+  },
+  {
+    "revision": "f3430a1cde56dcbd1e7d",
+    "url": "/static/js/2.848bfdd8.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/static/js/2.848bfdd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a089ca6d9fc228b79e1f",
+    "url": "/static/js/main.cbd4968b.chunk.js"
+  },
+  {
+    "revision": "07a84737f9aed538ce56",
+    "url": "/static/js/runtime-main.47729d7a.js"
+  },
+  {
+    "revision": "0450316ae00bf09e09610b7e6c7525a2",
+    "url": "/static/media/my_photo.0450316a.png"
+  }
+]);
